afba9bb9e59ef9e499cae782a6e99128a2fa3e6b
Eric Root <e70347@gmail.com>
2019-05-27 08:09:49 -0400

    now reads keys from file in production.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,23 @@
-module.exports = {
-  mongoURI: "mongodb://ecr:ecr1234@ds249035.mlab.com:49035/heroku_q9bf5k2b",
-  secret: "yoursecret",
-  registerKey: "needthiskey"
-};
+const fs = require("fs");
+
+var keys = {};
+
+if (process.env.NODE_ENV === "production") {
+  const private_key = fs.readFileSync("../private.key");
+  const register_key = fs.readFileSync("../register.key");
+  const db_key = fs.readFileSync("../db.key");
+
+  keys = {
+    mongoURI: db_key,
+    secret: private_key,
+    registerKey: register_key
+  };
+} else {
+  keys = {
+    mongoURI: "mongodb://ecr:ecr1234@ds249035.mlab.com:49035/heroku_q9bf5k2b",
+    secret: "yoursecret",
+    registerKey: "needthiskey"
+  };
+}
+
+module.exports = keys;
