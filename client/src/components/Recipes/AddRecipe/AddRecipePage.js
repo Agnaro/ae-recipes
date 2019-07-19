cb4d63b843f1bd278e5c12d40e1c0c7fb9961d6c
Eric Root <e70347@gmail.com>
2019-07-19 08:23:28 -0400

    Added thumbnail for selected picture and functions to prepare for upload

--- client/src/components/Recipes/AddRecipe/AddRecipePage.js
+++ client/src/components/Recipes/AddRecipe/AddRecipePage.js
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
 
 export class AddRecipePage extends Component {
+  state = {
+    recipe: [],
+    pic: ""
+  };
+
+  fileInput = React.createRef();
+
   changeHandler = evt => {
     this.setState({
-      meal: { ...this.state.meal, [evt.target.name]: evt.target.value }
+      recipe: { ...this.state.recipe, [evt.target.name]: evt.target.value }
+    });
+  };
+
+  submitHandler = async evt => {
+    evt.preventDefault();
+    const file = this.fileInput.current.files[0];
+    try {
+      const res = await this.readUploadedFile(file);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  fileOnChange = () => {
+    const file = this.fileInput.current.files[0];
+    this.setState({ pic: file ? window.URL.createObjectURL(file) : "" });
+  };
+
+  readUploadedFile = inputFile => {
+    const reader = new FileReader();
+
+    return new Promise((resolve, reject) => {
+      reader.onerror = () => {
+        reader.abort();
+        reject(new Error("Problem parsing input file."));
+      };
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsBinaryString(inputFile);
     });
   };
 
@@ -14,35 +52,53 @@ export class AddRecipePage extends Component {
         <form>
           <div>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={this.changeHandler}
+            />
           </div>
 
           <div>
             <label htmlFor="desc">Description</label>
-            <textarea id="desc" name="desc" />
+            <textarea id="desc" name="desc" onChange={this.changeHandler} />
           </div>
 
           <div>
             <label htmlFor="ingr">Ingredients</label>
-            <textarea id="ingr" name="ingr" />
+            <textarea id="ingr" name="ingr" onChange={this.changeHandler} />
           </div>
 
           <div>
             <label htmlFor="instr">Instructions</label>
-            <textarea id="instr" name="instr" />
+            <textarea id="instr" name="instr" onChange={this.changeHandler} />
           </div>
 
           <div>
             <label htmlFor="pic">Upload Picture</label>
-            <input type="file" id="pic" name="pic" />
+            <input
+              type="file"
+              id="pic"
+              name="pic"
+              accept="image/*"
+              ref={this.fileInput}
+              onChange={this.fileOnChange}
+            />
           </div>
+          <img src={this.state.pic} width="60px" />
 
           <div>
             <label htmlFor="link">Source</label>
-            <input type="url" id="link" name="link" />
+            <input
+              type="url"
+              id="link"
+              name="link"
+              onChange={this.changeHandler}
+            />
           </div>
 
-          <input type="button" value="Submit" />
+          <input type="button" value="Submit" onClick={this.submitHandler} />
         </form>
       </div>
     );
