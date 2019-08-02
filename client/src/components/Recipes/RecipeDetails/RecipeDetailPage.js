262a3a907cb35de0e25b0dc01b6738bf489e36cb
Eric Root <e70347@gmail.com>
2019-08-02 15:37:21 -0400

    Roughed out detail page

--- client/src/components/Recipes/RecipeDetails/RecipeDetailPage.js
+++ client/src/components/Recipes/RecipeDetails/RecipeDetailPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { server } from "../../../utils";
 import Axios from "axios";
+import "./recipeDetails.css";
 
 export class RecipeDetailPage extends Component {
   state = {
@@ -13,7 +14,6 @@ export class RecipeDetailPage extends Component {
 
   getRecipe() {
     Axios.get("/api/recipes/" + this.id).then(res => {
-      console.log(JSON.stringify(res.data));
       this.setState({
         recipe: res.data
       });
@@ -26,15 +26,32 @@ export class RecipeDetailPage extends Component {
 
   render() {
     const { recipe } = this.state;
+    const ingr = String(recipe.ingr).split("\n");
+    const instr = String(recipe.instr).split("\n\n");
     return (
-      <div style={{ textAlign: "center" }}>
+      <div className="page-box">
         <h2>{recipe.name}</h2>
         <img
           src={server + recipe.pic}
           alt="Prepared Meal"
           className="detail-img"
         />
+        <a href={recipe.link}>Source</a>
         <p>{recipe.desc}</p>
+
+        <h3>Ingredients</h3>
+        <ul className="ingr-list">
+          {ingr.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+
+        <h3>Instructions</h3>
+        <ol className="instr">
+          {instr.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
       </div>
     );
   }
